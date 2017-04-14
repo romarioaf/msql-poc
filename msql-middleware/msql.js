751e04c4ff23177a6d39914ec2ceab3ef60151a7
Romário Freitas <romarioaf1989@gmail.com>
2017-04-14 20:01:59 -0300

    Verificando se o serviço já estava registrado no comando count.

--- msql-middleware/msql.js
+++ msql-middleware/msql.js
@@ -1,5 +1,6 @@
 const redis = require("redis")
 const client = redis.createClient('6379', '127.0.0.1')
+const requestCount = require("./counter.js");
 
 module.exports = (req, resp, next) => {
 
@@ -14,21 +15,44 @@ module.exports = (req, resp, next) => {
     const register = req.get('MSQL-REGISTER')
     const unregister = req.get('MSQL-UNREGISTER')
     
-    if(register) {
-    	console.log("REGISTER");
+    console.log(`${req.headers.host}:${req.path}:COUNT`);
 
-    	client.incr(`${req.headers.host}:${req.path}:${register}`, function(err, data) {
-		  console.log("Incrementado...");
-		});
-    } else if (unregister) {
-    	console.log("UNREGISTER");
-    	console.log(unregister);
-    	
-    	client.del(`${req.headers.host}:${req.path}:${unregister}`, function(err, data) {
-    		console.log(`${req.headers.host}:${req.path}:${unregister}`);
+    if (unregister) {
+		console.log("UNREGISTER");
+		console.log(unregister);
+		
+		client.del(`${req.headers.host}:${req.path}:${unregister}`, function(err, data) {
+			console.log(`${req.headers.host}:${req.path}:${unregister}`);
 		    console.log(data);
 		});
-    }
+	} else {
+
+		//VERIFICANDO SE SERVIÇO CONTÉM COMANDO COUNT
+	    client.get(`${req.headers.host}:${req.path}:COUNT`, function(err, data) {
+		  console.log("Verificando se serviço foi registrado");
+		  if (data) {
+		  	console.log("Serviço já está sendo monitorado");
+		  	
+		  	client.incr(`${req.headers.host}:${req.path}:COUNT`, function(err, data) {
+			  console.log("Incrementado...");
+			});
+
+		  } else {
+		  	console.log("Serviço ainda não está sendo monitorado");
+
+		  	//REGISTRAR SERVIÇOS PARA SEREM MONITORADOS
+		  	if(register) {
+		    	console.log("REGISTER");
+
+		    	client.set(`${req.headers.host}:${req.path}:${register}`, 0, function(err, data) {
+				  console.log("Registrado");
+				});
+		    }
+		  }
+
+		});
+
+	}
     
     next()
 }
